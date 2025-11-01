d7afded7610ed6d784954a4e63a49296d625a69d
Volodymyr Sennikov <volodymyrsennikov@gmail.com>
2025-11-01 13:15:51 +0100

    functions for pagination and filtering events from db

--- infrastructure/event_db.go
+++ infrastructure/event_db.go
@@ -2,6 +2,8 @@ package infrastructure
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -41,14 +43,12 @@ func (r *EventRepository) GetEventByID(ctx context.Context, id int) (*services.E
 	if err := r.db.GetContext(ctx, &dbModel, query, id); err != nil {
 		return nil, err
 	}
-
 	event := toServiceEvent(dbModel)
 	return &event, nil
 }
 
 func (r *EventRepository) CreateEvent(ctx context.Context, params CreateEventParams) (int, error) {
 	var newID int
-
 	query := `
 	INSERT INTO events(event_datetime, description, _sport_id, _venue_id, _home_team_id, _away_team_id)
 	VALUES($1, $2, $3, $4, $5, $6)
@@ -59,10 +59,70 @@ func (r *EventRepository) CreateEvent(ctx context.Context, params CreateEventPar
 		params.EventDatetime, params.Description, params.SportID,
 		params.VenueID, params.HomeTeamID, params.AwayTeamID,
 	).Scan(&newID)
-
 	if err != nil {
 		return 0, err
 	}
 	return newID, nil
 }
 
+func (r *EventRepository) CountEvents(ctx context.Context, params ListEventsParams) (int, error) {
+	var args []interface{}
+	var total int
+	i := 1
+	whereQuery := []string{"1=1"}
+
+	if params.SportID != nil {
+		args = append(args, *params.SportID)
+		whereQuery = append(whereQuery, fmt.Sprintf("e._sport_id = $%d", i))
+		i++
+	}
+	if params.DateFrom != nil {
+		args = append(args, *params.DateFrom)
+		whereQuery = append(whereQuery, fmt.Sprintf("event_datetime >= $%d", i))
+		i++
+	}
+	query := fmt.Sprintf("SELECT COUNT(*) FROM events WHERE %s", strings.Join(whereQuery, " AND "))
+	if err := r.db.GetContext(ctx, &total, query, args...); err != nil {
+		return 0, err
+	}
+	return total, nil
+}
+
+func (r *EventRepository) ListEvents(ctx context.Context, params ListEventsParams) ([]services.Event, error) {
+	var dbModels []eventDBModel
+	var args []interface{}
+	i := 1
+	whereQuery := []string{"1=1"}
+
+	if params.SportID != nil {
+		args = append(args, *params.SportID)
+		whereQuery = append(whereQuery, fmt.Sprintf("e._sport_id = $%d", i))
+		i++
+	}
+	if params.DateFrom != nil {
+		args = append(args, *params.DateFrom)
+		whereQuery = append(whereQuery, fmt.Sprintf("e.event_datetime >= $%d", i))
+		i++
+	}
+	args = append(args, params.Limit)
+	limitClause := fmt.Sprintf("LIMIT $%d", i)
+	i++
+	args = append(args, params.Offset)
+	offsetClause := fmt.Sprintf("OFFSET $%d", i)
+	i++
+	query := fmt.Sprintf(
+		"%s WHERE %s ORDER BY e.event_datetime ASC %s %s",
+		baseEventSelectQuery,
+		strings.Join(whereQuery, " AND "),
+		limitClause,
+		offsetClause,
+	)
+	if err := r.db.SelectContext(ctx, &dbModels, query, args...); err != nil {
+		return nil, err
+	}
+	events := make([]services.Event, 0, len(dbModels))
+	for _, dbModel := range dbModels {
+		events = append(events, toServiceEvent(dbModel))
+	}
+	return events, nil
+}
